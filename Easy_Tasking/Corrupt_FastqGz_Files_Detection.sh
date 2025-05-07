52d23279c54642f4c4a04aae2a7e5d17392afc39
omprakash414 <omprakashs@iiitd.ac.in>
2025-05-07 16:11:03 +0530

    Faster version of zcat added

--- Easy_Tasking/Corrupt_FastqGz_Files_Detection.sh
+++ Easy_Tasking/Corrupt_FastqGz_Files_Detection.sh
@@ -1,5 +1,8 @@
-corrupted_files="corrupted_files.txt"
-for file in *.fastq.gz; do
-    zcat "$file" > /dev/null || echo "$file" >> "$corrupted_files"
-done
+#!/bin/bash
 
+corrupted_files="corrupted_files.txt"
+> "$corrupted_files"  # Clear the file
+
+for file in *.fastq.gz; do
+    pigz -p 25 -dc "$file" > /dev/null 2>/dev/null || echo "$file" >> "$corrupted_files"
+done
